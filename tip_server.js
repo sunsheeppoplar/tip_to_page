5622d98e25abfe402cd2412d7932eaf9cae4a01c
Abner Yang <abtyang@gmail.com>
2015-04-13 16:09:28 -0400

    server admin complete

--- tip_server.js
+++ tip_server.js
@@ -1,3 +1,5 @@
+//app requirements
+
 var express = require('express')
 var app = express();
 
@@ -10,7 +12,20 @@ app.use(bodyParser.urlencoded({extended: false}))
 var methodOverride = require('method-override')
 app.use(methodOverride('_method'))
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./db/blog.db')
 
+//routes
+
+//home redirect
+app.get('/', function(req, res) {
+	res.redirect('/home')
+});
+
+//show all posts
+app.get('/home', function(req, res) {
+	db.all("SELECT * FROM posts")
+})
 
 
 
